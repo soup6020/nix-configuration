8f8c03163552e5a4c762ddcd98e483f556e316b4
soup6020 <admin@soup6020.dev>
2025-02-06 18:39:00 -0800

    mpd config

--- modules/mpd.nix
+++ modules/mpd.nix
@@ -1,15 +1,32 @@
 {
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    extraConfig.pipewire.adjust-sample-rate = {
+      "context.properties" = {
+        "default.clock.rate" = 192000;
+        "default.allowed-rates" = [ 192000 ];
+      };
+    };
+  };
   services.mpd = {
     enable = true;
     musicDirectory = "/mnt/cold/Media/Music";
     user = "ezra";
     extraConfig = ''
-      # must specify one or more outputs in order to play audio!
-      # (e.g. ALSA, PulseAudio, PipeWire), see next sections
+        audio_output {
+        type "pipewire"
+        name "PipeWire Output"
+      }
     '';
 
-    # Optional:
     network.listenAddress = "any"; # if you want to allow non-localhost connections
     network.startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+  };
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/1000"; # hardcoded and ugly, should fix later
   };
 }
