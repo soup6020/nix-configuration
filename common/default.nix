68b6f483a0b8f8bff471a136ebbbf50224857549
soup6020 <ecallaghan@protonmail.com>
2025-02-10 13:43:12 -0800

    walker cachix

--- common/default.nix
+++ common/default.nix
@@ -11,11 +11,13 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nvf.cachix.org"
+      "https://walker-git.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
     ];
     experimental-features = [
       "nix-command"
