3236a7e2acc335c51b89d4de77ba8f8fcd088045
soup6020 <ecallaghan@protonmail.com>
2025-02-19 15:16:34 -0800

    fix capa

--- modules/custompkgs.nix
+++ modules/custompkgs.nix
@@ -2,6 +2,6 @@
 { config, pkgs, inputs, ... }:
 {
   environment.systemPackages = with pkgs; [
-        inputs.capa.packages.${pkgs.stdenv.hostPlatform.system}.capa
+        inputs.capa.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
