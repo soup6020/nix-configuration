dd9ac381104eea765992df3a6a7a7c496c776696
soup6020 <admin@soup6020.dev>
2025-03-02 16:40:10 -0800

    fix agenix path

--- modules/common/agenix.nix
+++ modules/common/agenix.nix
@@ -1,5 +1,5 @@
 {config, lib, pkgs, inputs, ... }:
 {
 environment.systemPackages = with pkgs; [ inputs.agenix.packages.${system}.default ];
-  age.secrets.mpdscribble.file = ../secrets/mpdscribble.age;
+  age.secrets.mpdscribble.file = ../../secrets/mpdscribble.age;
 }
