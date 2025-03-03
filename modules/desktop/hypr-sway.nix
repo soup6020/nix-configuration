34ba60f9bb1ac039df030cbf4ea484649c7e711a
soup6020 <ecallaghan@protonmail.com>
2025-03-03 01:26:17 -0800

    minor formatting change

--- modules/desktop/hypr-sway.nix
+++ modules/desktop/hypr-sway.nix
@@ -33,10 +33,9 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    withUWSM = true; # recommended for most users
-    xwayland.enable = true; # Xwayland can be disabled.
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    withUWSM = true;
+    xwayland.enable = true;
   };
 
   programs.sway = {
