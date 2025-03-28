7445dc83d4ac2f82135a79a1d07588c4278d6ebf
soup6020 <ecallaghan@protonmail.com>
2025-03-27 17:08:22 -0700

    libvirt changes

--- modules/sys/vm-oci.nix
+++ modules/sys/vm-oci.nix
@@ -10,7 +10,12 @@
   programs.virt-manager.enable = true;
   virtualisation.libvirtd = {
     enable = true;
-    qemu.ovmf.enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      ovmf.enable = true;
+      ovmf.packages = [ pkgs.OVMFFull.fd ];
+      swtpm.enable = true;
+    };
   };
   virtualisation.spiceUSBRedirection.enable = true;
 
@@ -21,4 +26,13 @@
     storageDriver = "btrfs";
     liveRestore = lib.mkForce false; # work around a strange issue where shutdowns hang, not ideal
   };
+  environment.etc = {
+  "ovmf/edk2-x86_64-secure-code.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+  };
+
+  "ovmf/edk2-i386-vars.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+  };
+ };
 }
