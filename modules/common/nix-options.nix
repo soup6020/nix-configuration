ccbc2ed7627803c963cba209a180edb1a67e2d34
soup6020 <ecallaghan@protonmail.com>
2025-04-16 09:22:18 -0700

    update flake.lock

--- modules/common/nix-options.nix
+++ modules/common/nix-options.nix
@@ -12,6 +12,7 @@
       "https://nix-community.cachix.org"
       "https://nvf.cachix.org"
       "https://walker-git.cachix.org"
+      "https://walker.cachix.org"
       "https://pwndbg.cachix.org"
       "https://ghostty.cachix.org"
     ];
@@ -19,6 +20,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nvf.cachix.org-1:GMQWiUhZ6ux9D5CvFFMwnc2nFrUHTeGaXRlVBXo+naI="
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
       "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
       "pwndbg.cachix.org-1:HhtIpP7j73SnuzLgobqqa8LVTng5Qi36sQtNt79cD3k="
       "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
