fdc0b9a7be157afb948d995dae66d3e2f29d7254
soup6020 <ecallaghan@protonmail.com>
2025-06-05 11:49:41 -0700

    nix-managed zsh plugins

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -5,7 +5,6 @@
   ...
 }:
 {
-  #TODO: programs.zsh.plugins appears to be a home-manager option, so this file doess not currently work.
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -20,37 +19,14 @@
       "INC_APPEND_HISTORY"
     ];
 
-    plugins = [
-      {
-        # Must be before plugins that wrap widgets, such as zsh-autosuggestions or fast-syntax-highlighting
-        name = "fzf-tab";
-        src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
-      }
-      {
-        name = "fast-syntax-highlighting";
-        src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-      }
-      {
-        name = "zsh-autosuggestions";
-        file = "zsh-autosuggestions.zsh";
-        src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
-      }
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "history-search-multi-word";
-        src = pkgs.zsh-history-search-multi-word;
-        file = "${pkgs.zsh-powerlevel10k}/share/zsh/zsh-history-search-multi-word/history-search-multi-word";
-      }
-      {
-        name = "history-substring-search";
-        src = pkgs.zsh-history-substring-search;
-        file = "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh";
-      }
-    ];
+    interactiveShellInit = ''
+    source ${pkgs.zsh-fzf-tab}/share/fzf-tab.plugin.zsh
+    source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh
+    source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+    source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+    source ${pkgs.zsh-history-search-multiword}/share/zsh/zsh-history-search-multi-word/history-search-multi-word.plugin.zsh
+    '';
 
   };
 }
