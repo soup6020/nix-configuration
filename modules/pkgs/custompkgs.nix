05614838fb2d0b9d53bf40984263792445211359
soup6020 <ecallaghan@protonmail.com>
2025-06-24 12:51:46 -0700

    update flake.lock

--- modules/pkgs/custompkgs.nix
+++ modules/pkgs/custompkgs.nix
@@ -6,7 +6,7 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    #    inputs.capa.packages.${pkgs.stdenv.hostPlatform.system}.default
+    #inputs.capa.packages.${pkgs.stdenv.hostPlatform.system}.default
     #inputs.rom64.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
