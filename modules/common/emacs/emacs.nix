6aad9f26f6b4f884fcf5fc5f22da06087be67241
soup6020 <ecallaghan@protonmail.com>
2025-10-22 21:06:33 -0700

    Format emacs pkgs

--- modules/common/emacs/emacs.nix
+++ modules/common/emacs/emacs.nix
@@ -19,38 +19,37 @@
       package = pkgs.emacs-unstable-pgtk;
       #Only define elisp pkgs here, LSPs and grammars must be under systemPackages
       extraEmacsPackages = epkgs: [
-        epkgs.use-package
         epkgs.cask
-        epkgs.vterm
         epkgs.eglot-booster
+        epkgs.use-package
+        epkgs.vterm
       ];
 
     })
     #More pkgs. Must be defined here for emacsclient to find them
-    pkgs.nixd
-    pkgs.libvterm
-    pkgs.shellcheck
-    pkgs.clang-tools
-    pkgs.ispell
-    pkgs.nixd
-    pkgs.go-grip
     pkgs.basedpyright
-    pkgs.sbcl
-    pkgs.yaml-language-server
-    pkgs.marksman
-    pkgs.rust-analyzer
+    pkgs.clang-tools
     pkgs.docker-language-server
-    pkgs.tree-sitter-grammars.tree-sitter-elisp
-    pkgs.tree-sitter-grammars.tree-sitter-nix
+    pkgs.go-grip
+    pkgs.ispell
+    pkgs.libvterm
+    pkgs.marksman
+    pkgs.nixd
+    pkgs.nixd
+    pkgs.rust-analyzer
+    pkgs.sbcl
+    pkgs.shellcheck
     pkgs.tree-sitter-grammars.tree-sitter-dockerfile
-    pkgs.tree-sitter-grammars.tree-sitter-nu
+    pkgs.tree-sitter-grammars.tree-sitter-elisp
+    pkgs.tree-sitter-grammars.tree-sitter-elisp
+    pkgs.tree-sitter-grammars.tree-sitter-llvm
     pkgs.tree-sitter-grammars.tree-sitter-markdown
     pkgs.tree-sitter-grammars.tree-sitter-markdown-inline
-    pkgs.tree-sitter-grammars.tree-sitter-llvm
-    pkgs.tree-sitter-grammars.tree-sitter-elisp
+    pkgs.tree-sitter-grammars.tree-sitter-nix
+    pkgs.tree-sitter-grammars.tree-sitter-nu
+    pkgs.yaml-language-server
   ];
   services.emacs = {
-    #Make the service available, but do not enable it by default
     install = true;
     enable = true;
     package = pkgs.emacs-unstable-pgtk;
